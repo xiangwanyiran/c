48e2b0dd16392feea36220dad36f68d8196ce597
Daluna <1175680996@qq.com>
2022-07-08 18:32:04 +0800

    day1

---
+++ day1.c
@@ -0,0 +1,31 @@
+/*
+ * @Author: Daluna 1175680996@qq.com
+ * @Date: 2022-07-08 10:23:51
+ * @LastEditors: Daluna 1175680996@qq.com
+ * @LastEditTime: 2022-07-08 14:52:55
+ * @FilePath: \c\day1.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+
+/* void print(char* str)
+{
+    printf("hehe:%s",str);
+}
+
+void test(void (*p)(char*))
+{
+    printf("********");
+    (*p)("ceshi");
+}
+
+int main()
+{
+    test(print);
+} */
+
+int main()
+{
+    printf ("Hello World");
+    return 0;
+}
